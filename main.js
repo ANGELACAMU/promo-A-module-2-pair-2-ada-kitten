6a4f2110745c3aa53e4229df7dc2c61fdb0edb8b
Crisferdi8 <fernandezcristina87@gmail.com>
2024-10-20 19:50:07 +0200

    add new files

--- main.js
+++ main.js
@@ -10,10 +10,8 @@ const kittenOne = `<li class="card">
               />
               <h3 class="card_title">Anastacio</h3>
               <h4 class="card_race">Siamés</h4>
-              <p class="card_description">
-                Porte elegante, su patrón de color tan característico y sus ojos
-                de un azul intenso, pero su historia se remonta a Asía al menos
-                hace 500 años, donde tuvo su origen muy posiblemente.
+              <p class="card_description">${kittenOneDesc}
+                
               </p>
             </article>
           </li>`;
@@ -27,10 +25,8 @@ const kittenTwo = `<li class="card">
             />
             <h3 class="card_title">Fiona</h3>
             <h4 class="card_race">Sphynx</h4>
-            <p class="card_description">
-              Produce fascinación y curiosidad. Exótico, raro, bello, extraño…
-              hasta con pinta de alienígena han llegado a definir a esta raza
-              gatuna que se caracteriza por la «ausencia» de pelo.
+            <p class="card_description">${kittenTwoDesc}
+              
             </p>
           </li>`;
 
@@ -42,19 +38,17 @@ const kittenThree = ` <li class="card">
             />
             <h3 class="card_title">Cielo</h3>
             <h4 class="card_race">Maine Coon</h4>
-            <p class="card_description">
-              Tienen la cabeza cuadrada y los ojos simétricos, por lo que su
-              bella mirada se ha convertido en una de sus señas de identidad.
-              Sus ojos son grandes y las orejas resultan largas y en punta.
+            <p class="card_description"> ${kittenThreeDesc}
+              
             </p>
           </li> `;
 
-listKittens.innerHTML = kittenOne + kittenTwo + kittenThree;
+// listKittens.innerHTML = kittenOne + kittenTwo + kittenThree;
 
 
-const buttonMenu = document.querySelector(".js-btn-add");
-const addCat = document.querySelector(".new-form");
-const buttonCancel = document.querySelector(".button-cancel")
+// const buttonMenu = document.querySelector(".js-btn-add");
+// const addCat = document.querySelector(".new-form");
+// const buttonCancel = document.querySelector(".button-cancel")
 
 
 
@@ -85,25 +79,47 @@ const buttonCancel = document.querySelector(".button-cancel")
 //tiene que mostrar el menú
 //seleccionar el botón del menú
 
-const buttonM = document.querySelector(".js-btn-add");
-const addC = document.querySelector(".new-form");
+// const buttonM = document.querySelector(".js-btn-add");
+// const addC = document.querySelector(".new-form");
 
-function showNewCatForm() {
-  addC.classList.remove('collapsed');
-}
-function hideNewCatForm() {
-  addC.classList.add('collapsed');
-}
-// showNewCatForm();
-buttonM.addEventListener('click', showNewCatForm);
-// buttonM.addEventListener('click', hideNewCatForm);
+// function showNewCatForm() {
+//   addC.classList.remove("collapsed");
+// }
+// function hideNewCatForm() {
+//   addC.classList.add("collapsed");
+// }
+// function handleClickNewCatForm() {
+//   if showNewCatForm();
+//   else hideNewCatForm();
+// }
 
-function handleClickNewCatForm(event) {
-  event.preventDeafault();
-  if (addC.classList.contains('collapsed'));
-  addC.classList.remove('collapsed'); {
-  } else {
-    addC.classList.remove('collapsed');
-  }
-}
+// buttonM.addEventListener("click", handleClickNewCatForm);
 
+
+
+
+const search = document.querySelector(".js_button-search");
+const desc = document.querySelector("js_in_search_desc");
+
+search.addEventListener("click", (ev) => {
+  ev.preventDefault();
+  const descSearchText = desc.value;
+  console.log("has hecho click");
+  console.log("Texto de búsqueda:", descSearchText);
+
+}); /*
+¿que falta?
+ */
+
+const kittenOneDesc =
+  "Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos
+hace 500 años, donde tuvo su origen muy posiblemente." 
+
+const kittenTwoDesc = "Produce fascinación y curiosidad. Exótico, raro, bello, extraño…
+              hasta con pinta de alienígena han llegado a definir a esta raza
+              gatuna que se caracteriza por la «ausencia» de pelo."
+
+
+const kittenThreeDesc = "Tienen la cabeza cuadrada y los ojos simétricos, por lo que su
+              bella mirada se ha convertido en una de sus señas de identidad.
+              Sus ojos son grandes y las orejas resultan largas y en punta."
